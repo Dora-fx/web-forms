d731612109abd21362668a22c82c9caa303a77e7
Dora-fx <dhzibovska@gmail.com>
2023-05-04 15:27:49 +0300

    forms done

--- close.js
+++ close.js
@@ -1,3 +1,11 @@
 closeBtn.onclick = function(){
     fixedForm.style.display = "none"
 }
+
+seePass.onclick = function (){
+    if (seePass.checked) {
+        pass.type = "text"
+    } else {
+        pass.type = "password"
+    }
+}
